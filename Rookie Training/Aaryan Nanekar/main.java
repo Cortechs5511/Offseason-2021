75675b42689155fa223f1ffcc955ac0812e35d53
Aaryan <68668582+Aaryanaan@users.noreply.github.com>
2021-11-14 19:11:42 -0500

    11/14 rookie training
    Added some notes and experimented a bit with java.

---
+++ Rookie Training/Aaryan Nanekar/main.java
@@ -0,0 +1,92 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // write whatever you want
+
+        /* 
+        double line comment
+
+        Normal comment but extended
+        Multiline Comment
+        */
+
+        // variables
+        int donkey = 20000;
+        donkey = donkey + 1;
+        System.out.println(donkey);
+        int height;
+
+        // naming
+        final int ROBOT_HEIGHT = 50;
+        System.out.println(ROBOT_HEIGHT);
+        int helloWorld = 5;
+
+        //string
+        String text = "good morning";
+        System.out.println(text);
+
+        // booleans
+        boolean buttonPressed = false;
+
+        // numbers
+        height = 0;
+        int letters = 26;
+        int animals = 3;
+        //int petals = 5.4; this won't work because 5.4 isn't an integer
+        double petals = 5.4; //works because double type allows decimals
+        final double pi = 3.1415;
+
+        //conditionals
+            if (donkey < 19999) {
+                System.out.println("not enough donkeys");
+            } else if(donkey == 19999) {
+                System.out.println("almost perfect amount of donkey");
+            } else if(donkey == 19999 && petals <= 5.4) {
+                System.out.println("wow pretty donkey");
+            } else {
+                System.out.println("Too many donkey");
+            };
+
+        // if have more than 19999 donkeys, ^^^ else ^^
+
+        // conditionals w/ grading
+        double grade = 78.3; 
+        if(grade <= 60) {
+            System.out.println("failure");
+        } else if(grade < 70) {
+            System.out.println("still a failure");
+        } else if(grade < 80) {
+            System.out.println("slightly smarter failure");
+        } else if(grade < 90) {
+            System.out.println("not better than failure but close");
+        } else if(grade < 100) {
+            System.out.println("ehhhhhhh");
+        } else if(grade == 100) {
+            System.out.println("Average");
+        };
+
+        /*
+        <, <=, >, >=, ==, !=, &&, ||
+        true AND true ---> true
+        true AND false ---> false
+        true OR true ---> true
+        true OR false ---> true
+        */
+
+        /*
+        Create BMI calculator
+        weight (kg) / height (meters) / height (in meters)
+
+        weight / height * height
+        */
+
+
+
+
+
+
+
+
+    }
+}
