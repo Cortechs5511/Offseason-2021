4955fec5bcc5e5ac7c3c3f4df63fc26461315f07
Aaditya-brrt <69121425+Aaditya-brrt@users.noreply.github.com>
2021-11-19 19:38:58 -0500

    Add files via upload

---
+++ Rookie Training/Aaditya/Main.java
@@ -0,0 +1,35 @@
+class Main {
+    public static void main(String[] args) {
+        /*
+        System.out.println("Hello World");
+        // write Hellow World
+        int age = 1;
+        age = age +1;
+        System.out.println(age);
+        final int WHEEL_DIAMETER = 30;
+        System.out.println(WHEEL_DIAMETER);
+        int yourmom = 32;
+        double yourdad = yourmom;
+        System.out.println(yourmom);
+        System.out.println(yourdad);
+        */
+        int animals = 44;
+        if (animals > 3) {
+            System.out.println("aopwgrhfijpiafjdp");
+        }
+        double grade = 78.3;
+        if (grade < 60) {
+            System.out.println("You failed");
+        }
+        /*
+        true and true is true
+        true and false is false
+        true or true is true
+        true or false is true
+        && AND
+        || OR
+        */
+
+
+    }
+}
