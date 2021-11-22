96701a0318527738b695556a077a03ac03bf3649
Aaditya-brrt <69121425+Aaditya-brrt@users.noreply.github.com>
2021-11-21 19:15:25 -0500

    Add files via upload

--- Rookie Training/Aaditya/Main.java
+++ Rookie Training/Aaditya/Main.java
@@ -12,7 +12,7 @@ class Main {
         double yourdad = yourmom;
         System.out.println(yourmom);
         System.out.println(yourdad);
-        */
+        
         int animals = 44;
         if (animals > 3) {
             System.out.println("aopwgrhfijpiafjdp");
@@ -20,6 +20,22 @@ class Main {
         double grade = 78.3;
         if (grade < 60) {
             System.out.println("You failed");
+        }
+        
+
+        int i = 0;
+        while (i<5) {
+            System.out.println(i);
+            i++;
+        }
+        for (int k = 0; k<5; k++) {
+            System.out.println(k);
+        }
+        */
+        String[] cars = {"Toyota", "Honda", "Lamborghini"};
+        System.out.println(cars.length);
+        for (String car: cars) {
+            System.out.println(car);
         }
         /*
         true and true is true
