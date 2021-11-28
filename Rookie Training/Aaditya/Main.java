2922c582d76d1b195b04d83c534fc926c4d7e3d7
Aaditya-brrt <69121425+Aaditya-brrt@users.noreply.github.com>
2021-11-28 18:39:15 -0500

    Worked on objects and methods

--- Rookie Training/Aaditya/Main.java
+++ Rookie Training/Aaditya/Main.java
@@ -1,28 +1,11 @@
 class Main {
+    static void myMethod(String name){
+        System.out.println(name);
+    }
     public static void main(String[] args) {
+        Main myObj = new Main();
+        myMethod("how are you doing");
         /*
-        System.out.println("Hello World");
-        // write Hellow World
-        int age = 1;
-        age = age +1;
-        System.out.println(age);
-        final int WHEEL_DIAMETER = 30;
-        System.out.println(WHEEL_DIAMETER);
-        int yourmom = 32;
-        double yourdad = yourmom;
-        System.out.println(yourmom);
-        System.out.println(yourdad);
-        
-        int animals = 44;
-        if (animals > 3) {
-            System.out.println("aopwgrhfijpiafjdp");
-        }
-        double grade = 78.3;
-        if (grade < 60) {
-            System.out.println("You failed");
-        }
-        
-
         int i = 0;
         while (i<5) {
             System.out.println(i);
@@ -31,13 +14,12 @@ class Main {
         for (int k = 0; k<5; k++) {
             System.out.println(k);
         }
-        */
+        
         String[] cars = {"Toyota", "Honda", "Lamborghini"};
         System.out.println(cars.length);
         for (String car: cars) {
             System.out.println(car);
         }
-        /*
         true and true is true
         true and false is false
         true or true is true
@@ -45,6 +27,8 @@ class Main {
         && AND
         || OR
         */
+        
+
 
 
     }
